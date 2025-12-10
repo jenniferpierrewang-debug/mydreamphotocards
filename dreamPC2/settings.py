45e5550cd3740577cee5918c37efe4fab8408db1
PIERRE Jennifer <jen@MacBook-Air-de-PIERRE.local>
2025-12-10 22:10:22 +0100

    Update start.sh and other configs for Render

--- dreamPC2/settings.py
+++ dreamPC2/settings.py
@@ -106,11 +106,11 @@ load_dotenv()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': os.getenv('DB_NAME', '').strip(),
+        'USER': os.getenv('DB_USER', '').strip(),
+        'PASSWORD': os.getenv('DB_PASSWORD', '').strip(),
+        'HOST': os.getenv('DB_HOST', '').strip(),
+        'PORT': os.getenv('DB_PORT', '').strip(),
     }
 }
 
