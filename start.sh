ad71b67ed6320d18819fb898233fc5e8be845ee4
PIERRE Jennifer <jen@MacBook-Air-de-PIERRE.local>
2025-12-10 21:34:28 +0100

    Update start.sh for Render deployment

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-source ../venv/bin/activate
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
 gunicorn dreamPC2.wsgi:application --bind 0.0.0.0:$PORT
