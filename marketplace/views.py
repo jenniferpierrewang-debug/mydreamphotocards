60c8bdc601914bee475d3f68811312e7389f3f0c
jenniferpierrewang-debug <jennifer.pierrewang@gmail.com>
2025-12-15 23:03:20 +0100

    Update views.py

--- marketplace/views.py
+++ marketplace/views.py
@@ -27,10 +27,11 @@ def listing_list_fr(request):
         listings = listings.filter(groupe__iexact=selected_group)
 
    # normalise tous les noms de groupes pour que majuscule/minuscule ne pose pas problème
-    groups = sorted([g.upper() for g in [
-        'BOYNEXTDOOR','SEVENTEEN','TXT','ENHYPEN','STRAY KIDS','XDH',
-        'AESPA','NCT','CORTIS','TWICE','MEOV','BLACKPINK','THE BOYZ','ZB1','ATEEZ'
-    ]])
+    groups = sorted([
+        'BOYNEXTDOOR','Seventeen','TXT','ENHYPEN','Stray Kids','XDH',
+        'AESPA','NCT','CORTIS','TWICE','MEOV','BLACKPINK','The Boyz','ZB1','ATEEZ'
+    ])
+
     grouped_listings = OrderedDict()
     for group_name in groups:
         grouped_listings[group_name] = listings.filter(groupe__iexact=group_name)
